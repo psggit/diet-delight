3512d3297cccf963aa93eecefaa750dd9c7c3444
Sachin Kumar Rajput <skr571999@gmail.com>
2021-03-30 00:49:14 +0530

    create seprate page for ForgotPassword

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ import SelectionCalenderMain from "./components/Dialog/Selection Calender Dialog
 import CalenderDialogMain from "./components/Dialog/Calender Dialog/CalenderDialogMain";
 import About from "./components/Our story page/About";
 import ImportMenu from './components/Admin/Menu/ImportMenu';
+import ForgotPassword from "./components/Auth/ForgotPassword";
 
 
 function App() {
@@ -116,6 +117,7 @@ function App() {
 
         <Route path="/signin" component={Signin} />
         <Route path="/signup" component={Signup} />
+        <Route path="/forgotpassword" exact component={ForgotPassword} />
         <Route path="/admin" exact component={AdminDash} />
         <Route path="/consultant" exact component={Consultant} />
         <Route path="/kitchen" exact component={Kitchen} />
