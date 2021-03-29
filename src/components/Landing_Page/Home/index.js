6d004421ae5822bfc0d24b76b693a8ff1c1be6c2
Sachin Kumar Rajput <skr571999@gmail.com>
2021-03-30 01:22:13 +0530

    add whatsapp web link

--- src/components/Landing_Page/Home/index.js
+++ src/components/Landing_Page/Home/index.js
@@ -6,6 +6,16 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
 
+    const messageOnWhatsAppWeb = () => {
+        const _adminMobile = process.env.REACT_APP_ADMIN_MOBILE;
+        const _message = "Hello, This is a Dummy Message";
+        const _whatsAppWebUrl = `https://api.whatsapp.com/send?phone=${_adminMobile}&text=${_message}`;
+        console.log("Mobile :", _adminMobile);
+        console.log("Mobile :", _whatsAppWebUrl);
+
+        window.open(_whatsAppWebUrl,'_blank')
+    };
+
     return (
         <div style={{position:"relative"}}>
             <HomeMain id="home">
@@ -34,7 +44,7 @@ const Home = () => {
                             <Message />
                         </Icon> */}
                         <Icon>
-                            <Whatsapp />
+                            <Whatsapp onClick={messageOnWhatsAppWeb} />
                         </Icon>
                     </Icons>
                 </HomeContainer>
